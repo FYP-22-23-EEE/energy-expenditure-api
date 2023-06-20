d94d69f17aaf109079969d124a95209ee3d62423
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2023-06-21 01:31:36 +0530

    activity updated

--- predict.py
+++ predict.py
@@ -9,8 +9,8 @@ import numpy as np
 class Activity(Enum):
     SITTING = 1
     STANDING = 2
-    WALKING = 3
-    RUNNING = 4
+    RUNNING = 3
+    CYCLING = 4
 
     @classmethod
     def from_string(cls, s):
@@ -18,15 +18,13 @@ class Activity(Enum):
             return cls.SITTING
         elif s.lower() == 'standing':
             return cls.STANDING
-        elif s.lower() == 'walking':
-            return cls.WALKING
         elif s.lower() == 'running':
             return cls.RUNNING
+        elif s.lower() == 'cycling':
+            return cls.CYCLING
 
     def __str__(self):
         return self.name.lower()
-
-    max: float
 
 
 class DeviceType(Enum):
@@ -82,20 +80,20 @@ class DataPoint:
 distributions_x = {
     Activity.SITTING: Distribution(mean=1.0, std=0.1, min=0.0, max=2.0),
     Activity.STANDING: Distribution(mean=3.0, std=0.1, min=2.1, max=4.0),
-    Activity.WALKING: Distribution(mean=5.0, std=0.3, min=4.1, max=7.0),
-    Activity.RUNNING: Distribution(mean=9.0, std=0.5, min=7.1, max=12.0)
+    Activity.RUNNING: Distribution(mean=5.0, std=0.3, min=4.1, max=7.0),
+    Activity.CYCLING: Distribution(mean=9.0, std=0.5, min=7.1, max=12.0)
 }
 distributions_y = {
     Activity.SITTING: Distribution(mean=1.0, std=0.1, min=12.1, max=14.0),
     Activity.STANDING: Distribution(mean=3.0, std=0.1, min=14.1, max=16.0),
-    Activity.WALKING: Distribution(mean=5.0, std=0.3, min=16.1, max=19.0),
-    Activity.RUNNING: Distribution(mean=9.0, std=0.5, min=19.1, max=24.0)
+    Activity.RUNNING: Distribution(mean=5.0, std=0.3, min=16.1, max=19.0),
+    Activity.CYCLING: Distribution(mean=9.0, std=0.5, min=19.1, max=24.0)
 }
 distributions_z = {
     Activity.SITTING: Distribution(mean=1.0, std=0.1, min=24.1, max=26.0),
     Activity.STANDING: Distribution(mean=3.0, std=0.1, min=26.1, max=28.0),
-    Activity.WALKING: Distribution(mean=5.0, std=0.3, min=28.1, max=31.0),
-    Activity.RUNNING: Distribution(mean=9.0, std=0.5, min=31.1, max=36.0)
+    Activity.RUNNING: Distribution(mean=5.0, std=0.3, min=28.1, max=31.0),
+    Activity.CYCLING: Distribution(mean=9.0, std=0.5, min=31.1, max=36.0)
 }
 
 
@@ -123,9 +121,9 @@ def predict(datapoints: list[DataPoint]) -> float:
         return random.uniform(0.75, 1.2)
     elif activity == Activity.STANDING:
         return random.uniform(0.80, 1.3)
-    elif activity == Activity.WALKING:
-        return random.uniform(3.0, 3.7)
     elif activity == Activity.RUNNING:
+        return random.uniform(3.0, 3.7)
+    elif activity == Activity.CYCLING:
         return random.uniform(6.0, 7.0)
     else:
         return random.uniform(0, 0.5)
